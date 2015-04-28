55ad1bb8051b1f1d277a5346d968a1fbb1242b1b
Eric W. Tramel <eric.tramel@gmail.com>
2015-04-28 11:44:32 +0200

    Empricial visible layer biasing from training set.

--- DBN/dbnsetup.m
+++ DBN/dbnsetup.m
@@ -9,7 +9,23 @@ function dbn = dbnsetup(dbn, x, opts)
         dbn.rbm{u}.W  = zeros(dbn.sizes(u + 1), dbn.sizes(u));
         dbn.rbm{u}.vW = zeros(dbn.sizes(u + 1), dbn.sizes(u));
 
-        dbn.rbm{u}.b  = zeros(dbn.sizes(u), 1);
+        if u==1
+            %%% Set Initial Visible Bias
+            % According to Hinton's RBM handbook, the initial visible bias
+            % should be set according to the log-ratio of the 1/0 probabilities
+            % of the training set.
+            p = mean(x,1);                          % Estimate P[x_i==1]
+            % We clip the probabilities to prevent Inf and NaN errors...
+            p(p > 1-(1e-8)) = 1-(1e-8);             % Clip upper value of probabilities
+            p(p < 1e-8) = 1e-8;                     % Clip lower value of probabilities
+            % Final bias assignment according to log probabilities
+            dbn.rbm{u}.b = log(p./(1-p))';        
+        else
+            % If this is the "lower" biasing for a deeper layer of the
+            % stacked RBM, then we should intialize the bias on this 
+            % hidden layer to zero.
+            dbn.rbm{u}.b = zeros(dbn.size(u), 1);
+        end
         dbn.rbm{u}.vb = zeros(dbn.sizes(u), 1);
 
         dbn.rbm{u}.c  = zeros(dbn.sizes(u + 1), 1);
