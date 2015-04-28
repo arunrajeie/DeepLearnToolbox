af7bc85da148648ca55a5439d8e1df61c834a822
Eric W. Tramel <eric.tramel@gmail.com>
2015-04-28 14:24:05 +0200

    Working Momentum.

--- DBN/dbnsetup.m
+++ DBN/dbnsetup.m
@@ -4,7 +4,15 @@ function dbn = dbnsetup(dbn, x, opts)
 
     for u = 1 : numel(dbn.sizes) - 1
         dbn.rbm{u}.alpha    = opts.alpha;
-        dbn.rbm{u}.momentum = opts.momentum;
+
+        %%% Momentum Transitioning
+        % From Hinton's Handbook, it is suggested that one should start
+        % with an initial momemntum level, perhaps at around 0.5 or so,
+        % and then after the learning has settled, to kick this up to 
+        % about 0.9 or so.
+        dbn.rbm{u}.momentum_initial = opts.momentum_initial;
+        dbn.rbm{u}.momentum_change_epoch = opts.momentum_change_epoch;
+        dbn.rbm{u}.momentum_final = opts.momentum_final;
 
         dbn.rbm{u}.W  = zeros(dbn.sizes(u + 1), dbn.sizes(u));
         dbn.rbm{u}.vW = zeros(dbn.sizes(u + 1), dbn.sizes(u));
